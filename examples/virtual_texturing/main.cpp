96b28ba7973ae6f7358b47d97a819e6b539b3168
Fabi3nn3 <fabienne.hubricht@uni-weimar.de>
2018-03-07 11:14:10 +0100

    virtual_texturing folder was missing

---
+++ examples/virtual_texturing/main.cpp
@@ -0,0 +1,141 @@
+/******************************************************************************
+* guacamole - delicious VR                                                   *
+*                                                                            *
+* Copyright: (c) 2011-2013 Bauhaus-Universität Weimar                        *
+* Contact:   felix.lauer@uni-weimar.de / simon.schneegans@uni-weimar.de      *
+*                                                                            *
+* This program is free software: you can redistribute it and/or modify it    *
+* under the terms of the GNU General Public License as published by the Free *
+* Software Foundation, either version 3 of the License, or (at your option)  *
+* any later version.                                                         *
+*                                                                            *
+* This program is distributed in the hope that it will be useful, but        *
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
+* or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License   *
+* for more details.                                                          *
+*                                                                            *
+* You should have received a copy of the GNU General Public License along    *
+* with this program. If not, see <http://www.gnu.org/licenses/>.             *
+*                                                                            *
+******************************************************************************/
+
+#include <functional>
+
+#include <gua/guacamole.hpp>
+#include <gua/renderer/TriMeshLoader.hpp>
+#include <gua/renderer/ToneMappingPass.hpp>
+#include <gua/renderer/SSAAPass.hpp>
+#include <gua/renderer/BBoxPass.hpp>
+#include <gua/renderer/DebugViewPass.hpp>
+#include <gua/renderer/VirtualTexturingPass.hpp>
+#include <scm/gl_util/manipulators/trackball_manipulator.h>
+
+#include <lamure/vt/ren/VTController.h>
+
+int main(int argc, char** argv) {
+
+  gua::init(argc, argv);
+
+  //setup scenegraph
+  gua::SceneGraph graph("main_scenegraph");
+  auto scene_transform = graph.add_node<gua::node::TransformNode>("/", "transform");
+
+  auto screen = graph.add_node<gua::node::ScreenNode>("/", "screen");
+  screen->data.set_size(gua::math::vec2(1.92f, 1.08f));
+  screen->translate(0, 0, 1.0);
+
+  //add mouse interaction
+  scm::gl::trackball_manipulator trackball;
+  trackball.transform_matrix()*scm::math::make_translation(0.01f, 0.002f, 0.2f);
+  trackball.dolly(0.2f);
+  float dolly_sens = 1.5f;
+  gua::math::vec2 trackball_init_pos(0.f);
+  gua::math::vec2 last_mouse_pos(0.f);
+  int button_state = -1;
+
+
+  //BEFORE ANY VT IS LOADED: TELL THE CONTROLLER HOW MUCH MEMORY SHOULD BE ALLOCATED FOR THE BUDGET!
+
+  lamure::vt::VTController* vt_controller_p = (lamure::vt::VTController*) lamure::vt::VTController::get_instance();
+  
+  vt_controller_p->cpu_budget_in_mb = 4096;
+  vt_controller_p->gpu_budget_in_mb = 1024;
+  vt_controller_p->cut_update_budget_in_mb = 10;
+
+
+  //LOAD TEXTURE ATLAS
+  gua::TextureDatabase::instance()->load("MYFILE.atlas");
+
+
+
+
+  //setup rendering pipeline and window
+  auto resolution = gua::math::vec2ui(1920, 1080);
+
+  auto camera = graph.add_node<gua::node::CameraNode>("/screen", "cam");
+  camera->translate(0, 0, 2.f);
+  camera->config.set_resolution(resolution);
+  
+  //use close near plane to allow inspection of details
+  camera->config.set_near_clip(0.01f);
+  camera->config.set_far_clip(20.0f);
+  camera->config.set_screen_path("/screen");
+  camera->config.set_scene_graph_name("main_scenegraph");
+  camera->config.set_output_window_name("main_window");
+  camera->config.set_enable_stereo(false);
+
+  auto pipe = std::make_shared<gua::PipelineDescription>();
+  pipe->add_pass(std::make_shared<gua::TriMeshPassDescription>());
+  pipe->add_pass(std::make_shared<gua::VirtualTexturingPassDescription>());
+  pipe->add_pass(std::make_shared<gua::LightVisibilityPassDescription>());
+  pipe->add_pass(std::make_shared<gua::ResolvePassDescription>());
+
+  camera->set_pipeline_description(pipe);
+
+  pipe->get_resolve_pass()->tone_mapping_exposure(5.f);
+
+  pipe->get_resolve_pass()->background_mode(gua::ResolvePassDescription::BackgroundMode::SKYMAP_TEXTURE);
+  pipe->get_resolve_pass()->background_texture("data/textures/envlightmap.jpg");
+
+  //init window and window behaviour
+  auto window = std::make_shared<gua::GlfwWindow>();
+  gua::WindowDatabase::instance()->add("main_window", window);
+  window->config.set_enable_vsync(false);
+  window->config.set_size(resolution);
+  window->config.set_resolution(resolution);
+  window->config.set_stereo_mode(gua::StereoMode::MONO);
+
+  window->on_resize.connect([&](gua::math::vec2ui const& new_size) {
+    window->config.set_resolution(new_size);
+    camera->config.set_resolution(new_size);
+    screen->data.set_size(gua::math::vec2(0.001 * new_size.x, 0.001 * new_size.y));
+  });
+
+
+
+
+  window->open();
+
+  gua::Renderer renderer;
+
+  //application loop
+  gua::events::MainLoop loop;
+  gua::events::Ticker ticker(loop, 1.0 / 500.0);
+
+  ticker.on_tick.connect([&]() {
+
+    window->process_events();
+    if (window->should_close()) {
+      renderer.stop();
+      window->close();
+      loop.stop();
+    }
+    else {
+      renderer.queue_draw({ &graph });
+    }
+  });
+
+  loop.start();
+
+  return 0;
+}
